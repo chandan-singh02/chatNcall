055ce833b76375a827070a165796408f8142bd7f
chandan-singh02 <chandanxingh173@gmail.com>
2024-06-15 10:39:43 +0530

    setupServer and middlewares

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+app.use(helmet());
+
+//parse json request url
+app.use(express.json());
+
+//parse json request body
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(mongoSanitize());
+
+app.use(cookieParser());
+
+app.use(compression());
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+app.use(cors());
+
+export default app;
